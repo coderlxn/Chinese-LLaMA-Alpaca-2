197c48e563eaad28b068169e61d2ccd044df1b2b
Jax <anyetiangong@qq.com>
2024-04-19 11:25:53 +0800

    Update run_sft.sh

--- scripts/training/run_sft.sh
+++ scripts/training/run_sft.sh
@@ -7,30 +7,31 @@ lora_trainable="q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
 modules_to_save="embed_tokens,lm_head"
 lora_dropout=0.05
 
-pretrained_model=path/to/hf/llama-2/or/chinese-llama-2/dir/or/model_id
-chinese_tokenizer_path=path/to/chinese-llama-2/tokenizer/dir
-dataset_dir=path/to/sft/data/dir
+pretrained_model="/root/autodl-fs/Llama-2-13b-hf"
+chinese_tokenizer_path="/root/autodl-fs/chinese-alpaca-2-lora-13b-16k"
+dataset_dir="/root/autodl-fs/train_source"
 per_device_train_batch_size=1
 per_device_eval_batch_size=1
 gradient_accumulation_steps=8
-max_seq_length=512
-output_dir=output_dir
-validation_file=validation_file_name
+max_seq_length=1024
+output_dir="/root/autodl-tmp/chinese-alpaca-2-13b-16k-sft"
+validation_file="/root/autodl-fs/val.json"
 
 deepspeed_config_file=ds_zero2_no_offload.json
 
-torchrun --nnodes 1 --nproc_per_node 1 run_clm_sft_with_peft.py \
+torchrun --nnodes 1 --nproc_per_node 6 run_clm_sft_with_peft.py \
     --deepspeed ${deepspeed_config_file} \
     --model_name_or_path ${pretrained_model} \
     --tokenizer_name_or_path ${chinese_tokenizer_path} \
     --dataset_dir ${dataset_dir} \
     --per_device_train_batch_size ${per_device_train_batch_size} \
     --per_device_eval_batch_size ${per_device_eval_batch_size} \
+    --peft_path "/root/autodl-fs/chinese-alpaca-2-lora-13b-16k"\
     --do_train \
     --do_eval \
     --seed $RANDOM \
     --fp16 \
-    --num_train_epochs 1 \
+    --num_train_epochs 5 \
     --lr_scheduler_type cosine \
     --learning_rate ${lr} \
     --warmup_ratio 0.03 \
@@ -54,9 +55,9 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_sft_with_peft.py \
     --trainable ${lora_trainable} \
     --lora_dropout ${lora_dropout} \
     --modules_to_save ${modules_to_save} \
-    --torch_dtype float16 \
     --validation_file ${validation_file} \
-    --load_in_kbits 16 \
+    --torch_dtype float16 \
+    --load_in_kbits 4 \
     --save_safetensors False \
     --gradient_checkpointing \
     --ddp_find_unused_parameters False
